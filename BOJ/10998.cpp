44829ed45293b595cb92599cc8750b66f1cf0f0c
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-08-23 23:42:08 +0900

    add 10998.cpp

---
+++ BOJ/10998.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a, b;
+    
+    // 입력
+    cin >> a >> b;
+    
+    // 출력
+    cout << a*b << "\n";
+    
+    return 0;
+    
+}
