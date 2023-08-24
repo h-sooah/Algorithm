822ed376fa6be02bc05113ccadaab26b2efa1d3e
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-08-24 23:41:33 +0900

    add 10869.cpp

---
+++ BOJ/10869.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a, b;
+    
+    // 입력
+    cin >> a >> b;
+    
+    // 출력
+    cout << a + b << "\n";
+    cout << a - b << "\n";
+    cout << a * b << "\n";
+    cout << a / b << "\n";
+    cout << a % b << "\n";
+    
+    return 0;
+    
+}
