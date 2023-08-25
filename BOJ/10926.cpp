294b1bb68ae7ab8f7b9a51e7cfab3ea60aa41d8f
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-08-25 21:01:06 +0900

    add 10926.cpp

---
+++ BOJ/10926.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    string s;
+    
+    // 입력
+    cin >> s;
+    
+    // 출력
+    cout << s << "??!\n";
+    
+    return 0;
+    
+}
