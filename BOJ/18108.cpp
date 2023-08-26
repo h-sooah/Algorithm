032950291d6cc7785065a045fb0c870c2da8f9db
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-08-26 23:14:18 +0900

    add 18108.cpp

---
+++ BOJ/18108.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int year;
+    
+    // 입력
+    cin >> year;
+    
+    // 출력
+    cout << year - 543 << "\n";
+    
+    return 0;
+    
+}
