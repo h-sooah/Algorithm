f39659dbda057a1f7fde5a701c64c7f444ade1c9
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-08-30 20:45:04 +0900

    add 11382.cpp

---
+++ BOJ/11382.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    long long a, b, c;
+    
+    // 입력
+    cin >> a >> b >> c;
+    
+    long long sum = a + b + c;
+    
+    // 출력
+    cout << sum << "\n";
+    
+    return 0;
+    
+}
