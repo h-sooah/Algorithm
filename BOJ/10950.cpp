8f15e7a13eb2a88bab537459eb47d36c08d56726
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-08-30 21:55:08 +0900

    add 10950.cpp

---
+++ BOJ/10950.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    int T, a, b;
+    
+    // 입력
+    cin >> T;
+    
+    // 연산 및 출력
+    while (T--) {
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+    
+}
