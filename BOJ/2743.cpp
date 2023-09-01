9004f0cb168f3666738522b562e11bd6f751de45
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-09-01 19:58:01 +0900

    add 2743.cpp

---
+++ BOJ/2743.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    string s;
+    
+    // 입력
+    cin >> s;
+    
+    // 연산 및 출력
+    cout << s.size() << "\n";
+    
+    return 0;
+    
+}
