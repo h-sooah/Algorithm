f28783e1e7e991841cb910c34c20c8321bf11fe0
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-09-01 20:08:39 +0900

    add 11654.cpp

---
+++ BOJ/11654.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    char c;
+    
+    // 입력
+    cin >> c;
+    
+    // 출력
+    cout << (int) c;
+    
+    return 0;
+    
+}
