ec9bbaa54342f341be5acd9d0852ffae7720d210
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-09-01 23:05:41 +0900

    add 25083.cpp

---
+++ BOJ/25083.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    
+    // 출력
+    cout << "         ,r'\"7" << "\n";
+    cout << "r`-_   ,'  ,/" << "\n";
+    cout << " \\. \". L_r'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+}
