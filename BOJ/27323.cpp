a2695043ce414c317c9759a0495d3f524eb49810
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-09-02 18:53:50 +0900

    add 27323.cpp

---
+++ BOJ/27323.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int A, B;
+    
+    // 입력
+    cin >> A >> B;
+    
+    // 출력
+    cout << A * B;
+    
+    return 0;
+}
