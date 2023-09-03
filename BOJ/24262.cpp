983e3c5de6948dbff0ec2875db4ff6bed02bd8db
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-09-03 12:58:29 +0900

    add 24262.cpp

---
+++ BOJ/24262.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int n;
+    
+    // 입력
+    cin >> n;
+    
+    // 출력
+    cout << 1 << "\n" << 0;
+    
+    return 0;
+}
