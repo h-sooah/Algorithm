7ba4999cf89f9fd0661295ad29e4ab2938a88fee
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-09-05 01:56:31 +0900

    add 1181.cpp

--- BOJ/1181.cpp
+++ BOJ/1181.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
-#include <set>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-struct compare {
-	bool operator() (const string& a, const string& b) const {
-		if (a.length() != b.length())
-			return a.length() < b.length();
-		return a < b;
-	}
-};
+bool comp (const string &a, const string &b) {
+    if (a.size() != b.size())
+        return a.size() < b.size();
+    return a < b;
+}
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
-	//입력
-	int N;
-	cin >> N;
-
-	set<string, compare> s;
-	string word;
-	while (N--) {
-		cin >> word;
-		s.insert(word);
-	}
-
-	for (auto iter : s)
-		cout << iter << "\n";
-
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    // 입력
+    int N;
+    cin >> N;
+    
+    // 연산 및 출력
+    vector<string> v;
+    string s;
+    while (N--) {
+        cin >> s;
+        v.push_back(s);
+    }
+    
+    sort(v.begin(), v.end(), comp);
+    
+    for (int i=0; i<v.size(); i++) {
+        if (i != 0 && v[i] == v[i-1])
+            continue;
+        cout << v[i] << "\n";
+    }
+    
+    return 0;
 }
