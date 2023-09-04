b740987f583ff289b385d2d2f5d0de0e83d6fb2e
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-09-05 00:14:37 +0900

    add 2750.cpp

--- BOJ/2750.cpp
+++ BOJ/2750.cpp
@@ -5,24 +5,26 @@
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
-	//입력
-	int N;
-	cin >> N;
-
-	vector<int> v;
-	int num;
-	while (N--) {
-		cin >> num;
-		v.push_back(num);
-	}
-
-	sort(v.begin(), v.end());
-
-	for (int i = 0; i < v.size(); i++)
-		cout << v[i] << "\n";
-
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    // 입력
+    int N;
+    cin >> N;
+    
+    vector<int> v;
+    int num;
+    while (N--) {
+        cin >> num;
+        v.push_back(num);
+    }
+    
+    sort(v.begin(), v.end());
+    
+    // 연산 및 출력
+    for (int i : v) {
+        cout << i << "\n";
+    }
+    
+    return 0;
 }
