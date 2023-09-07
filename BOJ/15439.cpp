2ea8ff548422e478825012bae212916515ed3335
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-09-08 01:04:07 +0900

    add 15439.cpp

---
+++ BOJ/15439.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    // 입력
+    int N;
+    cin >> N;
+    
+    // 연산 및 출력
+    cout << N * (N-1);
+    
+    return 0;
+}
