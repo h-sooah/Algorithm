8e94de488d454b9e8463eef359b94fa2bb093dca
sooah han <48647199+h-sooah@users.noreply.github.com>
2023-09-08 01:06:31 +0900

    add 24723.cpp

---
+++ BOJ/24723.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    // 입력
+    int N;
+    cin >> N;
+    
+    // 연산 및 출력
+    cout << pow(2, N);
+    
+    return 0;
+}
